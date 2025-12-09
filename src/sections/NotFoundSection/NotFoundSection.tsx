ef6bb3f58c65c53fd3d822f9a1b4a6918cbbe48d
Christian.Dev <CAquino.dev@gmail.com>
2025-12-09 17:32:14 +0800

    feat: use button from shadcn and use useNavigate for the back button.

--- src/sections/NotFoundSection/NotFoundSection.tsx
+++ src/sections/NotFoundSection/NotFoundSection.tsx
@@ -1,32 +1,32 @@
+import { Button } from '@/components/ui/button';
+import { useNavigate } from 'react-router';
 import illustration from '@/assets/404illustration.svg';
 
-
 const NotFoundSection = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col items-center justify-center my-15 px-4 text-center space-x-30 ">
-      <div>
-      </div>
+    <div className="flex flex-col items-center justify-center my-15 px-4 text-center space-x-30">
       <div className='flex flex-col justify-center items-center'>
         <img
           src={illustration}
           alt="Page Not Found Illustration"
           className="w-full max-w-sm mx-auto"
         />
-        <h1 className="text-6xl sm:text-7xl font-extrabold text-gray-800 mb-4">
+        <h3 className="text-6xl sm:text-7xl font-extrabold mb-4">
           404
-        </h1>
-        <p className="text-lg sm:text-2xl text-gray-600 mb-6">
+        </h3>
+        <p className="text-lg sm:text-2xl mb-6">
           Oops! The page you are looking for does not exist.
         </p>
-        <button
-          onClick={() => window.history.back()}
-          className="px-6 py-3 bg-primary text-primary-foreground hover:bg-primary/90 transition w-50 rounded-md"
+        <Button
+          onClick={() => navigate(-1)}
         >
           Go Back
-        </button>
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
-export default NotFoundSection
+export default NotFoundSection;
