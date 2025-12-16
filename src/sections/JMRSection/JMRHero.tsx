42538dd95439e80244ab1ad812529a8d30f39738
Christian.Dev <CAquino.dev@gmail.com>
2025-12-16 14:11:00 +0800

    fix: use lazy image and fix font size in jmr hero section

--- src/sections/JMRSection/JMRHero.tsx
+++ src/sections/JMRSection/JMRHero.tsx
@@ -2,6 +2,7 @@ import CoverflowCarousel from "@/components/jmr-components/CoverflowCarousel";
 import chairhero from "@/assets/JMR/hero-section/chairhero.png";
 import chairhero1 from "@/assets/JMR/hero-section/chairhero-1.png";
 import chairhero2 from "@/assets/JMR/hero-section/chairhero-2.png";
+import LazyImage from "@/components/LazyImage";
 
 const JMRHero: React.FC = () => {
   const images = [
@@ -20,30 +21,30 @@ const JMRHero: React.FC = () => {
       <h3 className="text-2xl md:text-4xl font-semibold">
         Your Trusted Customs <br /> Brokerage Partner
       </h3>
-      <p className="mt-2 text-lg md:text-xl">Fast. Reliable. Efficient</p>
+      <p className="mt-2 text-sm md:text-base"> <i>Fast. Reliable. Efficient</i></p>
 
       {/* Desktop Grid - Centered */}
       <div className="hidden md:flex justify-center mt-5">
         <div className="grid grid-cols-2 grid-rows-2 gap-4 h-120 w-180">
           <div className="col-span-1 row-span-2 h-full">
-            <img
+            <LazyImage
               src={chairhero}
               alt="chairhero"
-              className="h-full w-full object-cover"
+              effect="blur"
             />
           </div>
           <div className="col-span-1">
-            <img
+            <LazyImage
               src={chairhero1}
               alt="chairhero1"
-              className="h-full w-full object-cover"
+              effect="blur"
             />
           </div>
           <div className="col-span-1">
-            <img
+            <LazyImage
               src={chairhero2}
               alt="chairhero2"
-              className="h-full w-full object-cover"
+              effect="blur"
             />
           </div>
         </div>
